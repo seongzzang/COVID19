09544a430665543671e173bd0c316f8fa5d2f224
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-08-03 22:04:32 +0900

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 양성혜 on 2022/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
