07c5390e40d109e6477332898eae65a7069a2b6e
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-08-03 22:12:11 +0900

    main_update

--- ViewController.swift
+++ ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
